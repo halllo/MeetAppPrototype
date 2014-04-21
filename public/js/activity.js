000d32f80647fc308d552a57602aa8942af0fd64
Manuel Naujoks <mnaujoks@gmail.com>
2014-04-21 21:59:10 +0200

    First version of offline frontend complete.

--- public/js/activity.js
+++ public/js/activity.js
@@ -1,26 +1,49 @@
 function ActivityController($scope) {
+  
+  var utility = new Utility(_);
+
+
+  $scope.loginCredentials = {
+    username:   '',
+    password:   ''
+  };
+
+  $scope.loggedin = false;
+
+  $scope.login = function() {
+    if ($scope.loginCredentials.username)
+    {
+      $scope.loggedin = true;
+    }
+    else
+    {
+      $scope.loggedin = false;
+    }
+  };
+
+
   $scope.activities = [
     {
-      name:'learn angular', 
-      location:'Karlsruhe',
-      date:'2014.04.21',
-      friends:'friend1, friend2'
+      name:     'watch a movie', 
+      location: 'Karlsruhe',
+      date:     '2014.04.21',
+      friends:  ['Friend1','Friend2']
     }
   ];
 
   $scope.newActivity = {
-    name:'', 
-    location:'',
-    date:'',
-    friends:''
+    name:     '', 
+    location: '',
+    date:     '',
+    friends:  ''
   };
  
   $scope.addActivity = function() {
     $scope.activities.push({
-      name:$scope.newActivity.name,
-      location:$scope.newActivity.location,
-      date:$scope.newActivity.date,
-      friends:$scope.newActivity.friends
+      name:     $scope.newActivity.name,
+      location: $scope.newActivity.location,
+      date:     $scope.newActivity.date,
+      friends:  utility.split($scope.newActivity.friends)
     });
   
     $scope.newActivity.name = '';
@@ -28,5 +51,8 @@ function ActivityController($scope) {
     $scope.newActivity.date = '';
     $scope.newActivity.friends = '';
   };
- 
+
+  $scope.getFriends = function(activity) {
+    return activity.friends.join(', ');
+  }
 }
--- public/js/utility.js
+++ public/js/utility.js
@@ -1,8 +1,19 @@
-function Utility(_){
-	split = function(s){
-		var csv = s.replace(/\sand\s/g, ", ");
-		return _.map(csv.split(','), function(i){ 
-			return i.replace(/^\s+|\s+$/g, "")
-		});
-	} 
-}
+(function() {
+
+  var Utility = function(underscore) {
+  	this.underscore = underscore
+  };
+
+  Utility.prototype.split = function(s){
+	var csv = s.replace(/\sand\s/g, ", ");
+	return this.underscore.map(csv.split(','), function(i){ 
+		return i.replace(/^\s+|\s+$/g, "")
+	});
+  };
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Utility;
+  else
+    window.Utility = Utility;
+
+})();
